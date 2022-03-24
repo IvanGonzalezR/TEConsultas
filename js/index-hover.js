2ecff5aa0a9551f9de34583fc8be79331718ad90
IvanG <kikin-gr2000@hotmail.com>
2022-03-24 12:26:43 -0600

    antes del Rulas merge

--- js/index-hover.js
+++ js/index-hover.js
@@ -1,4 +1,12 @@
-function changeImage() {
+/*function changeImage() {
     var image1 = document.getElementById('img1');
+    var image2 = document.getElementById('img2');
+    var image3 = document.getElementById('img3');
+    var image4 = document.getElementById('img4');
     image1.src = "/recursos/img/cuadro1_hover.png";
-}
+    image2.src = "/recursos/img/cuadro2_hover.png";
+    image3.src = "/recursos/img/cuadro3_hover.png";
+    image4.src = "/recursos/img/cuadro4_hover.png";
+
+    if (image1)
+}*/

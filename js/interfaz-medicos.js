03fa103f5a900dac193a362cf02a934b66759408
ManuelRdzz <73724469+ManuelRdzz@users.noreply.github.com>
2022-03-24 01:00:54 -0600

    Avance
    Avance de la pagina de interfaz medicos

--- js/interfaz-medicos.js
+++ js/interfaz-medicos.js
@@ -1,6 +1,1 @@
-const btnOcultar = document.querySelector('#ocultar');
-const menu = document.querySelector('menu');
 
-btnOcultar.addEventListener('click', ()=>{
-    menu.setAttribute('style','display:none');
-})

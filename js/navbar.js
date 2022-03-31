7a505af8068a6df35e7a7fc67e9a73f208916191
Raul Galindo <raulgasa97@gmail.com>
2022-03-31 16:22:12 -0600

    Agrege search doctors.

--- js/navbar.js
+++ js/navbar.js
@@ -1,19 +1,18 @@
 //NAVBAR MOBILE
-const navOpen = document.querySelector(".nav__open");
-const navClose = document.querySelector(".nav__close");
-const navMobileList = document.querySelector(".nav__mobile__links");
-const navWebList = document.querySelector(".nav__web__links");
+const navLines = document.querySelector(".nav__lines");
+const navLinesX = document.querySelector(".nav__linesX"); 
+const navMobileList = document.querySelector(".nav__mobile");
 
 // Metodo para abrir el menu en mobile.
-   navOpen.addEventListener("click", function () {
+   navLines.addEventListener("click", function () {
+       navLines.style.display = "none";
+       navLinesX.style.display = "flex";     
        navMobileList.style.display = "flex";
-       navOpen.style.display = "none";
-       navClose.style.display = "block";
    });
 
 // Metodo para cerrar el menu en mobile.
-   navClose.addEventListener("click", function () {
+   navLinesX.addEventListener("click", function () {
+       navLines.style.display = "flex";
+       navLinesX.style.display = "none";     
        navMobileList.style.display = "none";
-       navOpen.style.display = "block";
-       navClose.style.display = "none";
    });

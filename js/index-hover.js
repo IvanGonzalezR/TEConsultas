550587ffe8c5e9b31e1a3fff253f928d094a3971
JuanmaAlfaro <alu.18131212@correo.itlalaguna.edu.mx>
2022-03-31 20:51:50 -0600

    se agerego el hover del index
    se usaron 8 funciones papá

--- js/index-hover.js
+++ js/index-hover.js
@@ -1,12 +1,27 @@
-/*function changeImage() {
-    var image1 = document.getElementById('img1');
-    var image2 = document.getElementById('img2');
-    var image3 = document.getElementById('img3');
-    var image4 = document.getElementById('img4');
-    image1.src = "/recursos/img/cuadro1_hover.png";
-    image2.src = "/recursos/img/cuadro2_hover.png";
-    image3.src = "/recursos/img/cuadro3_hover.png";
-    image4.src = "/recursos/img/cuadro4_hover.png";
+//-----------------------Funciones onmouseover-------------------------------
+function changeImage1() {
+    let image1 = document.getElementById('img1');
+    image1.src = "/recursos/img/cuadro1_hover.png"; }
+function changeImage2() {
+    let image2 = document.getElementById('img2');
+    image2.src = "/recursos/img/cuadro2_hover.png"; }
+function changeImage3() {
+    let image3 = document.getElementById('img3');
+    image3.src = "/recursos/img/cuadro3_hover.png"; }
+function changeImage4() {
+    let image4 = document.getElementById('img4');
+    image4.src = "/recursos/img/cuadro4_hover.png"; }
 
-    if (image1)
-}*/
+//-----------------------Funciones onmouseout----------------------------------
+function changeImageOut1(){
+    let image1 = document.getElementById('img1');
+    image1.src = "recursos/img/cuadro1.png"; }
+function changeImageOut2(){
+    let image2 = document.getElementById('img2');
+    image2.src = "recursos/img/cuadro2.png"; }
+function changeImageOut3(){
+    let image3 = document.getElementById('img3');
+    image3.src = "recursos/img/cuadro3.png"; }
+function changeImageOut4(){
+    let image4 = document.getElementById('img4');
+    image4.src = "recursos/img/cuadro4.png"; }

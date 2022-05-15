266d4fa0085c5d5ed032d7a827fcd9310e667f53
IvanG <kikin-gr2000@hotmail.com>
2022-05-15 01:14:40 -0500

    Registro terminado, login a medias

--- js/registros.js
+++ js/registros.js
@@ -1,3 +1,5 @@
+let esDoctor = false;
+
 const clicked = function(elem){
     document.getElementById(elem).style.backgroundColor = "#495057";
 }
@@ -13,6 +15,7 @@ document.getElementById("doctor").addEventListener("click", function(event){
     event.preventDefault();
     clicked("doctor");
     notClicked("paciente");
+    esDoctor = true;
 
     if(!document.getElementById("cedula")){
         const inpCedula = document.createElement("input");
@@ -30,6 +33,7 @@ document.getElementById("paciente").addEventListener("click", function(event){
     event.preventDefault();
     clicked("paciente");
     notClicked("doctor");
+    esDoctor = false;
 
     if(document.getElementById("cedula")){
         const inputs = document.querySelector(".inputs");
@@ -43,12 +47,19 @@ crearCuenta.addEventListener("click", function(event){
     event.preventDefault();
     let paciente = {};
     // Obtener datos del formulario
-    if(true){
+    if(!esDoctor){
         paciente.nombre = document.getElementById("nombre").value;
         paciente.correo = document.getElementById("correo").value;
         paciente.password = document.getElementById("contrasena").value;
         console.log(paciente);
         crearPaciente(paciente);
+    }else{
+        paciente.nombre = document.getElementById("nombre").value;
+        paciente.correo = document.getElementById("correo").value;
+        paciente.password = document.getElementById("contrasena").value;
+        paciente.cedula = document.getElementById("cedula").value;
+        console.log(paciente);
+        crearDoctor(paciente);
     }
 });
 
@@ -56,9 +67,26 @@ function crearPaciente(paciente){
     const headers = {"Content-Type": "application/json",};
     axios.post('http://localhost:3005/api/pacientes/registro', paciente, {headers})
         .then(response => {
-            // const users = response.data.data;
-            // console.log(GET list users, users);
-            
+            console.log(response);
+            alert("Usuario registrado con exito");
+            setTimeout( function() { window.location.href = "./login.html"; }, 1000 );
         })
-        .catch(error => console.error(error));
+        .catch(error => {console.error(error)
+        if (error.response.status === 401){
+            alert("El correo ya esta registrado");
+        }});
+}
+
+function crearDoctor(doctor){
+    const headers = {"Content-Type": "application/json",};
+    axios.post('http://localhost:3005/api/doctores/registro', doctor, {headers})
+        .then(response => {
+            console.log(response);
+            alert("Doctor registrado con exito");
+            setTimeout( function() { window.location.href = "./login.html"; }, 1000 );
+        })
+        .catch(error => {console.error(error)
+        if (error.response.status === 401){
+            alert("El correo ya esta registrado");
+        }});
 }
